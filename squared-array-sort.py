8f913f00e44e1d132320c47dad3ac9e3956f4d43
xiunhon <tien.nguyen10@student.csulb.edu>
2021-10-21 10:36:27 -0700

    Update squared-array-sort.py
    Optimal solution, better runtime

--- squared-array-sort.py
+++ squared-array-sort.py
@@ -1,10 +1,11 @@
 def sortedSquares(nums):
+    # helper function that merge 2 sorted arrays together
     def merge_2_arr(arr1, arr2):
         result = [None]*(len(arr1)+len(arr2))
         arr1_ptr = 0
         arr2_ptr = 0
         result_ptr = 0
-            
+
         while arr1_ptr < len(arr1) and arr2_ptr < len(arr2):
             if arr1[arr1_ptr]**2 < arr2[arr2_ptr]**2:
                 result[result_ptr] = arr1[arr1_ptr]
@@ -22,28 +23,28 @@ def sortedSquares(nums):
             result[result_ptr] = arr2[arr2_ptr]
             arr2_ptr+=1
             result_ptr+=1
-            
+
         return result
-        
-    def merge_sort_helper(nums, start_index, end_index):
-        if start_index >= end_index:
-            return
-        median = (start_index + end_index) // 2
-        merge_sort_helper(nums, start_index, median)
-        merge_sort_helper(nums,median+1,end_index)
-        
-        left = nums[start_index:median+1]
-        right = nums[median+1:end_index+1]
-        
-        sorted_subarr = merge_2_arr(left, right)
-        
-        for i in range(end_index - start_index + 1):
-            nums[start_index + i] = sorted_subarr[i]
-    
-    merge_sort_helper(nums, 0, len(nums)-1)
-    for i in range(len(nums)):
-        nums[i] = nums[i]**2
-    return nums
+
+    #find non-negative and negative part of sorted array
+    #negative part is already sorted.
+    #just need to be reversed and put absolute values into a new array, eg:[-10,,-5,-3]->[3,5,10]
+    i = 0
+    while i != len(nums):
+        if nums[i] >= 0:
+            break
+        i += 1
+    non_negative = nums[i:]
+    negative = [None]*(i)
+    while i != 0:
+        negative[len(negative)-i] = abs(nums[i-1])
+        i -= 1
+
+    #use merge_2_arr to merge 2 sorted arrays together
+    squared_arr = merge_2_arr(negative,non_negative)
+    for i in range(len(squared_arr)):
+        squared_arr[i] = squared_arr[i]**2
+    return squared_arr
 
 nums = [-10,-2,0,1,3]
 print(sortedSquares(nums))
